7c8325021aaa8e58ad46bcfa119cc815d859b328
Gaëtan Melchilsen <Gatan Melchilsen>
2014-03-04 17:13:16 +0100

    Vérif .pdf sur fichier expert en JS

--- src/HopitalNumerique/UserBundle/Resources/public/js/Expert/script.js
+++ src/HopitalNumerique/UserBundle/Resources/public/js/Expert/script.js
@@ -20,3 +20,55 @@ function deleteAllReponses(path)
         }
     });
 }
+
+function checkTypeFile()
+{
+//      var file = $('input[type="file"]').val();
+//      var exts = ['pdf'];
+//      
+//      // first check if file field has any value
+//      if ( file ) {
+//        // split file name at dot
+//        var get_ext = file.split('.');
+//        // reverse name to check extension
+//        get_ext = get_ext.reverse();
+//        // check file type is valid as given in 'exts' array
+//        if ( $.inArray ( get_ext[0].toLowerCase(), exts ) > -1 )
+//        {
+//          alert( 'Allowed extension!' );
+//        } 
+//        else 
+//        {
+//        	apprise( 'Veuillez selectionner un pdf' );
+//        }
+//      }
+		
+	  var validation = true;
+	
+      //Récupération de tout les champs de type file
+      var files = $('input[type="file"]');
+      //tableau des extensions autorisées
+      var exts = ['pdf'];
+      
+      for (var key in files)
+      {
+    	  var file = files[key];
+    	  var lib  = file.value;
+    	  if('' == lib || null == lib) 
+    		  continue;
+    	  
+    	  //Récupère l'extention
+          var get_ext = lib.split('.');
+          get_ext = get_ext.reverse();
+          var extValide = $.inArray ( get_ext[0].toLowerCase(), exts ) > -1;
+          if ( !extValide )
+          {
+        	  alert(lib + ' n\' est pas un pdf !');
+        	  validation =false;
+          }
+      }
+      
+      if( validation )
+    	  $('form').submit();
+      
+}
