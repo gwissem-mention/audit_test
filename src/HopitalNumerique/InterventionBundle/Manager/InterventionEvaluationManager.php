9bb5720a431ce58c28ba0171b8023a80e917196d
Rémi LECLERC <rleclerc@nodevo.com>
2014-03-18 10:04:59 +0100

    suppr entité intervention_evaluation

--- src/HopitalNumerique/InterventionBundle/Manager/InterventionEvaluationManager.php
+++ src/HopitalNumerique/InterventionBundle/Manager/InterventionEvaluationManager.php
@@ -7,7 +7,6 @@
 namespace HopitalNumerique\InterventionBundle\Manager;
 
 use Symfony\Component\Security\Core\SecurityContext;
-use Nodevo\AdminBundle\Manager\Manager as BaseManager;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use HopitalNumerique\InterventionBundle\Entity\InterventionDemande;
@@ -17,10 +16,8 @@ use HopitalNumerique\InterventionBundle\Manager\InterventionCourrielManager;
 /**
  * Manager pour les évaluations des demandes d'intervention.
  */
-class InterventionEvaluationManager extends BaseManager
+class InterventionEvaluationManager
 {
-    protected $_class = 'HopitalNumerique\InterventionBundle\Entity\InterventionEvaluation';
-
     /**
      * @var \Symfony\Component\Security\Core\SecurityContext SecurityContext de l'application
      */
@@ -47,9 +44,8 @@ class InterventionEvaluationManager extends BaseManager
      * @param \HopitalNumerique\InterventionBundle\Manager\InterventionCourrielManager $interventionCourrielManager Le manager de l'entité InterventionCourriel
      * @return void
      */
-    public function __construct(EntityManager $entityManager, SecurityContext $securityContext, Router $router, InterventionCourrielManager $interventionCourrielManager)
+    public function __construct(SecurityContext $securityContext, Router $router, InterventionCourrielManager $interventionCourrielManager)
     {
-        parent::__construct($entityManager);
         $this->securityContext = $securityContext;
         $this->router = $router;
         $this->interventionCourrielManager = $interventionCourrielManager;
