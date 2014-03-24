f8ccc0d687861bcc759b9ac83827a2e459f7d845
Thomas DAURIAC <tdauriac@nodevo.com>
2014-03-24 11:06:14 +0100

    script ok

--- script.sql
+++ script.sql
@@ -8,14 +8,14 @@
    PROD -> DEV
    Lorem ipsum */
 
-UPDATE `core_mail` SET `mail_body` = 'Bonjour %u, %candidat vient de soumettre une candidature ambassadeur. Voici ses informations : %questionnaire Cordialement,' WHERE `core_mail`.`mail_id` = 24;
+/*UPDATE `core_mail` SET `mail_body` = 'Bonjour %u, %candidat vient de soumettre une candidature ambassadeur. Voici ses informations : %questionnaire Cordialement,' WHERE `core_mail`.`mail_id` = 24;
 
 /* Modiciation de l’ordre des items suivants : */
-UPDATE `core_menu_item` SET `itm_order` = '3' WHERE `core_menu_item`.`itm_id` = 70;
+/*UPDATE `core_menu_item` SET `itm_order` = '3' WHERE `core_menu_item`.`itm_id` = 70;
 UPDATE `core_menu_item` SET `itm_order` = '4' WHERE `core_menu_item`.`itm_id` = 71; 
 UPDATE `core_menu_item` SET `itm_order` = '5' WHERE `core_menu_item`.`itm_id` = 72;
 UPDATE `core_menu_item` SET `itm_order` = '6' WHERE `core_menu_item`.`itm_id` = 73;
-UPDATE `core_menu_item` SET `itm_order` = '7' WHERE `core_menu_item`.`itm_id` = 74;
+UPDATE `core_menu_item` SET `itm_order` = '7' WHERE `core_menu_item`.`itm_id` = 74;*/
 
 /* Insertion en 2nd place du ‘modifier mon mot de passe’ */
 INSERT INTO `core_menu_item` (`itm_id`, `itm_parent`, `mnu_menu`, `itm_name`, `itm_route`, `itm_route_parameters`, `itm_route_absolute`, `itm_uri`, `itm_icon`, `itm_display`, `itm_display_children`, `itm_role`, `itm_order`) VALUES
