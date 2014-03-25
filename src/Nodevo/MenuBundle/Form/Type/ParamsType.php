1b6187a354f0d476b057080fda14bc27b53623dd
Quentin SOMAZZI <qsomazzi@gmail.com>
2014-03-25 15:46:03 +0100

    fix bug theme forms

--- src/Nodevo/MenuBundle/Form/Type/ParamsType.php
+++ src/Nodevo/MenuBundle/Form/Type/ParamsType.php
@@ -5,8 +5,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-use Doctrine\ORM\EntityRepository;
-
 class ParamsType extends AbstractType
 {
     private $_params = array();
