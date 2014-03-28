695d87b9a2165d1ec285dad5ed449cf174bfd59b
Rémi LECLERC <rleclerc@nodevo.com>
2014-03-28 15:33:56 +0100

    menu intervention admin

--- script.sql
+++ script.sql
@@ -14,6 +14,25 @@
    PROD -> DEV
    Lorem ipsum */
 
+INSERT INTO `core_menu_item` (
+`itm_id` ,
+`itm_parent` ,
+`mnu_menu` ,
+`itm_name` ,
+`itm_route` ,
+`itm_route_parameters` ,
+`itm_route_absolute` ,
+`itm_uri` ,
+`itm_icon` ,
+`itm_display` ,
+`itm_display_children` ,
+`itm_role` ,
+`itm_order`
+)
+VALUES (
+NULL , '54', '1', 'Gestion des interventions', 'hopital_numerique_intervention_admin_liste', NULL , '0', NULL , NULL , '1', '0', 'IS_AUTHENTICATED_ANONYMOUSLY', '3'
+);
+
 
 ---------------------------------------------------------------------------------------------
 /* QSO - 24/03/2014
