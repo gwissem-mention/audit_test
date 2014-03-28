7e6be3e8a1092ce78fe33dbada6ef673a8e57d58
Rémi LECLERC <rleclerc@nodevo.com>
2014-03-28 17:04:42 +0100

    sous-menu admin intervention

--- script.sql
+++ script.sql
@@ -32,6 +32,42 @@ INSERT INTO `core_menu_item` (
 VALUES (
 NULL , '54', '1', 'Gestion des interventions', 'hopital_numerique_intervention_admin_liste', NULL , '0', NULL , NULL , '1', '0', 'IS_AUTHENTICATED_ANONYMOUSLY', '3'
 );
+INSERT INTO `core_menu_item` (
+`itm_id` ,
+`itm_parent` ,
+`mnu_menu` ,
+`itm_name` ,
+`itm_route` ,
+`itm_route_parameters` ,
+`itm_route_absolute` ,
+`itm_uri` ,
+`itm_icon` ,
+`itm_display` ,
+`itm_display_children` ,
+`itm_role` ,
+`itm_order`
+)
+VALUES (
+NULL , '104', '1', 'Éditer des interventions', 'hopital_numerique_intervention_admin_demande_edit', NULL , '0', NULL , NULL , '1', '0', 'IS_AUTHENTICATED_ANONYMOUSLY', '1'
+);
+INSERT INTO `core_menu_item` (
+`itm_id` ,
+`itm_parent` ,
+`mnu_menu` ,
+`itm_name` ,
+`itm_route` ,
+`itm_route_parameters` ,
+`itm_route_absolute` ,
+`itm_uri` ,
+`itm_icon` ,
+`itm_display` ,
+`itm_display_children` ,
+`itm_role` ,
+`itm_order`
+)
+VALUES (
+NULL , '104', '1', 'Voir une évaluation', 'hopital_numerique_intervention_admin_evaluation_voir', NULL , '0', NULL , NULL , '1', '0', 'IS_AUTHENTICATED_ANONYMOUSLY', '2'
+);
 
 
 ---------------------------------------------------------------------------------------------
