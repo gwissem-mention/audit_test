b39da272ef09c744f19b51fea5cb3ba48ef0b6ff
Thomas DAURIAC <tdauriac@nodevo.com>
2014-03-31 12:11:33 +0200

    script ok

--- script.sql
+++ script.sql
@@ -14,35 +14,6 @@
    PROD -> DEV
    Lorem ipsum */
 
-INSERT INTO `core_ressource` (
-`res_id` ,
-`res_nom` ,
-`res_pattern` ,
-`res_order` ,
-`res_type`
-)
-VALUES (
-'23', 'BackOffice - Gestion des interventions', '/^\\/admin\\/intervention/', '23', '1'
-);
-UPDATE `core_menu_item` SET `itm_name` = 'Éditer une intervention' WHERE `core_menu_item`.`itm_id` =105;
-INSERT INTO `core_menu_item` (
-`itm_id` ,
-`itm_parent` ,
-`mnu_menu` ,
-`itm_name` ,
-`itm_route` ,
-`itm_route_parameters` ,
-`itm_route_absolute` ,
-`itm_uri` ,
-`itm_icon` ,
-`itm_display` ,
-`itm_display_children` ,
-`itm_role` ,
-`itm_order`
-)
-VALUES (
-NULL , '104', '1', 'Voir une intervention', 'hopital_numerique_intervention_admin_demande_voir', NULL , '0', NULL , NULL , '1', '0', 'IS_AUTHENTICATED_ANONYMOUSLY', '3'
-);
 
 ---------------------------------------------------------------------------------------------
 /* QSO - 24/03/2014
