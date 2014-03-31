20b60a67c230f8e12d6d8fac89e1d6729d8ebd53
Rémi LECLERC <rleclerc@nodevo.com>
2014-03-31 12:36:54 +0200

    #833

--- script.sql
+++ script.sql
@@ -14,6 +14,14 @@
    PROD -> DEV
    Lorem ipsum */
 
+ALTER TABLE `hn_intervention_demande` DROP FOREIGN KEY `FK_6DEA57304F81EDEA` ;
+ALTER TABLE `hn_intervention_regroupement` DROP FOREIGN KEY `FK_39B795AD7DDE15` ;
+ALTER TABLE `hn_intervention_regroupement` DROP FOREIGN KEY `FK_39B795B56A61A6` ;
+ALTER TABLE `hn_intervention_regroupement` DROP FOREIGN KEY `FK_39B7952B07AECF` ;
+ALTER TABLE `hn_intervention_etablissement_rattache` DROP FOREIGN KEY `FK_DA1B4928C7F0ABC4` ;
+ALTER TABLE `hn_intervention_ambassadeur_historique` DROP FOREIGN KEY `FK_1D720677C7F0ABC4` ;
+ALTER TABLE `hn_intervention_evaluation` DROP FOREIGN KEY `FK_64C51B55C7F0ABC4` ;
+ALTER TABLE `hn_intervention_objet` DROP FOREIGN KEY `FK_7845C1B3C7F0ABC4` ;
 
 ---------------------------------------------------------------------------------------------
 /* QSO - 24/03/2014
