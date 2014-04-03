f77c56b418498a9d32af7b45445d17dcc222d403
Sebastien Delamare <sdelamare@nodevo.com>
2014-04-03 13:45:13 +0200

    Anomalie #823 : Ergonomie écran de référencement.

--- src/HopitalNumerique/ObjetBundle/Resources/public/js/script.js
+++ src/HopitalNumerique/ObjetBundle/Resources/public/js/script.js
@@ -338,17 +338,23 @@ function manageCollapse(element, way)
 function updateNbChilds()
 {
     $('#references-tab .ref').each(function(){
-        childs    = $(this).data('childs');
+        childs			= $(this).data('childs');
+		parentLevel		= $(this).data('level');
         nbChecked = 0;
-
+		nbChildsDirect = 0;
+		
         if( childs.length > 0 ) {
             $.each(childs,function(key, val){
 
-                if ( $('.ref-'+val+' .checkbox').prop('checked') )
+                if ( $('.ref-'+val+' .checkbox').prop('checked') && $('.ref-'+val).data('level') == parentLevel + 1 )
                     nbChecked++
+				
+				if ( $('.ref-'+val).data('level') == parentLevel + 1 )
+                    nbChildsDirect++
             });
         }
 
         $(this).find('.nbChilds').html( nbChecked );
+		$(this).find('.nbChildsDirect').html( nbChildsDirect );
     })
 }
