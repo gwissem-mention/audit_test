38427e519db01086f462bc1206a9a47fecd32c2d
Gaëtan Melchilsen <Gatan Melchilsen>
2014-04-04 12:14:59 +0200

    script.sql de contact

--- script.sql
+++ script.sql
@@ -4,11 +4,11 @@
 
 
 ---------------------------------------------------------------------------------------------
-/* GME - 13/02/2014
+/* GME - 04/04/2014
    PROD -> DEV
    Lorem ipsum */
 
-
+UPDATE `core_menu_item` SET `itm_route` = 'hopital_numerique_contact_homepage', `itm_uri` = NULL WHERE `core_menu_item`.`itm_id` = 61;
 ---------------------------------------------------------------------------------------------
 /* RLE - 28/02/2014
    PROD -> DEV
