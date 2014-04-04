90227190b887e5edfb7f4d9cfe9bd9b6ac8826a9
Quentin SOMAZZI <qsomazzi@gmail.com>
2014-04-04 15:54:50 +0200

    fix for insight

--- src/HopitalNumerique/PublicationBundle/Twig/PublicationExtension.php
+++ src/HopitalNumerique/PublicationBundle/Twig/PublicationExtension.php
@@ -9,10 +9,10 @@ class PublicationExtension extends \Twig_Extension
     /**
      * Construit l'extension Twig
      */
-    public function __construct($managerObjet, $managerContenu)
+    public function __construct($container)
     {
-        $this->_managerObjet   = $managerObjet;
-        $this->_managerContenu = $managerContenu;
+        $this->_managerObjet   = $container->get('hopitalnumerique_objet.manager.objet');
+        $this->_managerContenu = $container->get('hopitalnumerique_objet.manager.contenu');
     }
 
     /**
@@ -26,7 +26,7 @@ class PublicationExtension extends \Twig_Extension
             'parsePublication' => new \Twig_Filter_Method($this, 'parsePublication')
         );
     }
-  
+
     /**
      * Parse le contenu pour créer les liens vers les publications
      *
@@ -42,54 +42,52 @@ class PublicationExtension extends \Twig_Extension
         // matches[0] tableau des chaines completes trouvée
         // matches[1] tableau des chaines avant les : trouvé
         // matches[1] tableau des ID après les : trouvé
-        if(is_array($matches[1])){
-          foreach($matches[1] as $key => $value){
-            switch($value){
-              case 'PUBLICATION':
-                    //cas Objet
-                    $objet = $this->_managerObjet->findOneBy( array( 'id' => $matches[2][$key] ) );
-                    $target = $matches[4][$key] == "1" ? "target='_blank'" : "";
-                    if($objet)
-                        $replacement = '<a href="/publication/'.$matches[2][$key].'-' . $objet->getAlias() . '" '.$target.'>' . $matches[3][$key] . '</a>';
-                    else
-                        $replacement = "<a href=\"javascript:alert('Cette publication n\'existe pas')\" ".$target.">" . $matches[3][$key] . ' </a>';
+        if(is_array($matches[1]))
+        {
+            foreach($matches[1] as $key => $value)
+            {
+                switch($value){
+                    case 'PUBLICATION':
+                        //cas Objet
+                        $objet  = $this->_managerObjet->findOneBy( array( 'id' => $matches[2][$key] ) );
+                        $target = $matches[4][$key] == "1" ? "target='_blank'" : "";
+                        if($objet)
+                            $replacement = '<a href="/publication/'.$matches[2][$key].'-' . $objet->getAlias() . '" '.$target.'>' . $matches[3][$key] . '</a>';
+                        else
+                            $replacement = "<a href=\"javascript:alert('Cette publication n\'existe pas')\" ".$target.">" . $matches[3][$key] . ' </a>';
 
-                    $pattern = $matches[0][$key];
-                    
-                    
-                    $content = str_replace($pattern, $replacement, $content);
-                    
-                break;
-              case 'INFRADOC':
-                //cas contenu
-                $contenu = $this->_managerContenu->findOneBy( array( 'id' => $matches[2][$key] ) );
-                $target = $matches[4][$key] == "1" ? "target='_blank'" : "";
-                if( $contenu ){
-                    $objet       = $contenu->getObjet();
-                    $replacement = '<a href="/publication/'.$objet->getId().'-' . $objet->getAlias() . '/'.$matches[2][$key].'-'.$contenu->getAlias().'" '.$target.'>' . $matches[3][$key].'</a>';
-                }else
-                    $replacement = "<a href=\"javascript:alert('Cet infra-doc n\'existe pas')\" ".$target.">" . $matches[3][$key].'</a>';
+                        $pattern = $matches[0][$key];
+                        $content = str_replace($pattern, $replacement, $content);
+                                
+                        break;
+                    case 'INFRADOC':
+                        //cas contenu
+                        $contenu = $this->_managerContenu->findOneBy( array( 'id' => $matches[2][$key] ) );
+                        $target  = $matches[4][$key] == "1" ? "target='_blank'" : "";
+                        if( $contenu ){
+                            $objet       = $contenu->getObjet();
+                            $replacement = '<a href="/publication/'.$objet->getId().'-' . $objet->getAlias() . '/'.$matches[2][$key].'-'.$contenu->getAlias().'" '.$target.'>' . $matches[3][$key].'</a>';
+                        }else
+                            $replacement = "<a href=\"javascript:alert('Cet infra-doc n\'existe pas')\" ".$target.">" . $matches[3][$key].'</a>';
 
-                $pattern = $matches[0][$key];
-                $content = str_replace($pattern, $replacement, $content);
-                break;
-              case 'ARTICLE':
-                    //cas Objet
-                    $objet = $this->_managerObjet->findOneBy( array( 'id' => $matches[2][$key] ) );
-                    $target = $matches[4][$key] == "1" ? "target='_blank'" : "";
-                    if($objet)
-                        $replacement = '<a href="/publication/article/'.$matches[2][$key].'-' . $objet->getAlias() . '" '.$target.'>' . $matches[3][$key] . '</a>';
-                    else
-                        $replacement = "<a href=\"javascript:alert('Cet article n\'existe pas')\" ".$target.">" . $matches[3][$key] . ' </a>';
+                        $pattern = $matches[0][$key];
+                        $content = str_replace($pattern, $replacement, $content);
+                        break;
+                    case 'ARTICLE':
+                        //cas Objet
+                        $objet  = $this->_managerObjet->findOneBy( array( 'id' => $matches[2][$key] ) );
+                        $target = $matches[4][$key] == "1" ? "target='_blank'" : "";
+                        if($objet)
+                            $replacement = '<a href="/publication/article/'.$matches[2][$key].'-' . $objet->getAlias() . '" '.$target.'>' . $matches[3][$key] . '</a>';
+                        else
+                            $replacement = "<a href=\"javascript:alert('Cet article n\'existe pas')\" ".$target.">" . $matches[3][$key] . ' </a>';
 
-                    $pattern = $matches[0][$key];
-                    
-                    
-                    $content = str_replace($pattern, $replacement, $content);
-                    
-                break;
+                        $pattern = $matches[0][$key];
+                        $content = str_replace($pattern, $replacement, $content);
+                                
+                        break;
+                }
             }
-          }
         }
         
         return $content;
