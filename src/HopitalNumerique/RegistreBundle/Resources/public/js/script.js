c46b4d15290fa4af0c785d21b35d6adaea3a4d08
Gaëtan Melchilsen <Gatan Melchilsen>
2014-04-07 16:49:54 +0200

    Correctif actualité + registre ambassadeur

--- src/HopitalNumerique/RegistreBundle/Resources/public/js/script.js
+++ src/HopitalNumerique/RegistreBundle/Resources/public/js/script.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
     //fancybox liste des outils maitrisés
     $('a.link').fancybox({
         'padding'   : 0,
-        'autoSize'  : true,
-        'scrolling' : 'yes'
+        'scrolling' : 'auto',
+        'width' 	: '600px',
+        'height' 	: 'auto'
     });
 });
 
