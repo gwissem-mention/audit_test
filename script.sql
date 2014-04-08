dc8bb82e9a5ba81ac329636dd64cafb00f01c74c
Thomas DAURIAC <tdauriac@nodevo.com>
2014-04-08 16:54:30 +0200

    scriptok

--- script.sql
+++ script.sql
@@ -8,7 +8,7 @@
    PROD -> DEV
    Lorem ipsum */
 
-UPDATE `core_menu_item` SET `itm_route` = 'hopital_numerique_contact_homepage', `itm_uri` = NULL WHERE `core_menu_item`.`itm_id` = 61;
+
 ---------------------------------------------------------------------------------------------
 /* RLE - 28/02/2014
    PROD -> DEV
