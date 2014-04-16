51321ec10935c38ec099cd09b27076cd3084a76f
Gaëtan Melchilsen <Gatan Melchilsen>
2014-04-16 15:01:21 +0200

    Ajout du DoctrineMigrationBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -49,6 +49,7 @@ class AppKernel extends Kernel
             new Nodevo\GestionnaireMediaBundle\NodevoGestionnaireMediaBundle(),
             new Nodevo\FaqBundle\NodevoFaqBundle(),
             new HopitalNumerique\FaqBundle\HopitalNumeriqueFaqBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
