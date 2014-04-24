270e3b16c36fd877b7443a1bc8ee74eab022f0a9
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2014-04-24 14:06:15 +0200

    Correctif modules

--- src/HopitalNumerique/ModuleBundle/Repository/SessionRepository.php
+++ src/HopitalNumerique/ModuleBundle/Repository/SessionRepository.php
@@ -39,7 +39,7 @@ class SessionRepository extends EntityRepository
             ->leftJoin('ses.etat','refEtat')
             ->leftJoin('ses.module','module')
             ->leftJoin('ses.inscriptions', 'inscriptions', Join::WITH, 'inscriptions.etatInscription = :idAcccepte')
-            ->setParameter('idAcccepte', 407)
+            ->setParameter('idAcccepte', 332)
             ->where( 'module.id = :idModule')
             ->setParameter('idModule', $condition->value )
             ->orderBy('ses.dateSession');
