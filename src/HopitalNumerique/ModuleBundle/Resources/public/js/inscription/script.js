1568958997d9f8f9a61cbfb4bf01b8298aa96b39
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2014-05-20 19:00:40 +0200

    Refus inscription

--- src/HopitalNumerique/ModuleBundle/Resources/public/js/inscription/script.js
+++ src/HopitalNumerique/ModuleBundle/Resources/public/js/inscription/script.js
@@ -13,9 +13,11 @@ $(function () {
             if ( $('.grid_massactions select option:selected').text() == 'Refuser inscription' )
             {
                 apprise('Quelle est la raison du refus ?(Copiez une des raisons suivantes dans le champ ci-dessous)<br /><br /><ul><li>La session a été annulée faute de participants</li><li>L\'animateur de la session a été contraint d\'annuler sa participation</li><li>La session est complète. Nous vous recommandons de vous positionner rapidement sur une prochaine date</li></ul>', 
-                    {'input':true}, function(r){
-                        $.cookie('textMailInscription', r);
-                        $('.grid form').submit();
+                    {'input':true,'textOk':'Valider le refus','textCancel':'Annuler'}, function(r) {
+                        if(r) { 
+                            $.cookie('textMailInscription', r);
+                            $('.grid form').submit();
+                        }
                 });
             }else
                 $('.grid form').submit();
