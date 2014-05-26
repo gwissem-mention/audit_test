3178638593eb8d1cba3e6ab52396c1b87732b432
Quentin SOMAZZI <qsomazzi@gmail.com>
2014-05-26 10:24:20 +0200

    add onglet autodiag sur les users

---
+++ src/HopitalNumerique/UserBundle/Controller/ResultatController.php
@@ -0,0 +1,24 @@
+<?php
+namespace HopitalNumerique\UserBundle\Controller;
+
+use HopitalNumerique\UserBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Controller des Résultats par utilisateurs
+ */
+class ResultatController extends Controller
+{
+    public function indexAction( User $user )
+    {
+        $resultats = $this->get('hopitalnumerique_autodiag.manager.resultat')->findBy( array('user'=>$user) );
+
+        return $this->render( 'HopitalNumeriqueUserBundle:Resultat:index.html.twig' , array(
+            'user'      => $user,
+            'options'   => $this->get('hopitalnumerique_user.gestion_affichage_onglet')->getOptions($user),
+            'resultats' => $resultats
+        ));
+    }
+}
