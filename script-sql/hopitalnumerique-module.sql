c2aa2f06e56418c4460b86a0c7490006e50e412a
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2014-06-09 16:48:49 +0200

    Ajout du script + correctif nom route

---
+++ script-sql/hopitalnumerique-module.sql
@@ -0,0 +1,3 @@
+INSERT INTO `core_menu_item` (`itm_id`, `itm_parent`, `mnu_menu`, `itm_name`, `itm_route`, `itm_route_parameters`, `itm_route_absolute`, `itm_uri`, `itm_icon`, `itm_display`, `itm_display_children`, `itm_role`, `itm_order`)
+VALUES
+    (NULL, 128, 1, 'Gestion des inscriptions', 'hopitalnumerique_module_module_allinscription', '[]', NULL, NULL, NULL, 1, 0, 'IS_AUTHENTICATED_ANONYMOUSLY', 3);
