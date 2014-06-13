58a2b183358ba307501457ee715c07efaa906084
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2014-06-13 16:25:34 +0200

    Module inscription action mass

---
+++ script-sql/hopitalnumerique-module.sql
@@ -0,0 +1,3 @@
+INSERT INTO `hn_reference` (`ref_id`, `parent_id`, `ref_libelle`, `ref_code`, `ref_etat`, `ref_dictionnaire`, `ref_recherche`, `ref_lock`, `ref_order`)
+VALUES
+    (425, NULL, 'NA', 'ETAT_EVALUATION', 3, 0, 0, 1, 4);
