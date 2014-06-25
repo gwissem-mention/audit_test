8342a05eb418aaf6343e052f72251d516896993f
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2014-06-25 17:57:29 +0200

    Forum : autorisation des anonymous, we are anonymous, we are legion

--- src/HopitalNumerique/ForumBundle/Entity/Category.php
+++ src/HopitalNumerique/ForumBundle/Entity/Category.php
@@ -3,6 +3,7 @@
 namespace HopitalNumerique\ForumBundle\Entity;
 
 use CCDNForum\ForumBundle\Entity\Category as BaseCategory;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * 
@@ -11,5 +12,31 @@ use CCDNForum\ForumBundle\Entity\Category as BaseCategory;
  */
 class Category extends BaseCategory
 {
-        
+    /**
+     *
+     * @param  SecurityContextInterface $securityContext
+     * @return bool
+     */
+    public function isAuthorisedToRead(SecurityContextInterface $securityContext)
+    {
+        if (0 == count($this->readAuthorisedRoles)) {
+            return true;
+        }
+
+        foreach ($this->readAuthorisedRoles as $role) 
+        {
+            if ("anon."  === $securityContext->getToken()->getUser())
+            {
+                if ('ROLE_ANONYME_10' === $role) 
+                {
+                    return true;
+                }
+            }
+            elseif ($securityContext->isGranted($role)) {
+                return true;
+            }
+        }
+
+        return false;
+    } 
 }
