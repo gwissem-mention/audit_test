ed647ef34e1288a83797ea6fc4a57f978e26ee2e
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2014-07-07 12:26:38 +0200

    Forum : suppression cap 255 caract

--- src/HopitalNumerique/ForumBundle/Form/Type/User/Post/PostUpdateFormType.php
+++ src/HopitalNumerique/ForumBundle/Form/Type/User/Post/PostUpdateFormType.php
@@ -61,7 +61,7 @@ class PostUpdateFormType extends AbstractType
                     'label'              => 'post.body-label',
                     'translation_domain' => 'CCDNForumForumBundle',
                     'attr'               => array(
-                        'class' => 'validate[required,minSize[15],maxSize[255]]',
+                        'class' => 'validate[required,minSize[15]',
                         'rows' => 8,
                         'acl_group' => 'default'
                     )
