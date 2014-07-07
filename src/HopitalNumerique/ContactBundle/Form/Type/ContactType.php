c2e4bdc523d7ff5072fdc8d8282d45e426acf807
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2014-07-07 18:19:55 +0200

    Contact

--- src/HopitalNumerique/ContactBundle/Form/Type/ContactType.php
+++ src/HopitalNumerique/ContactBundle/Form/Type/ContactType.php
@@ -16,9 +16,9 @@ class ContactType extends NodevoContactType
 {
     private $_constraints = array();
 
-    public function __construct($manager, $validator)
+    public function __construct($manager, $validator, $securityContext)
     {
-        parent::__construct($manager, $validator);
+        parent::__construct($manager, $validator, $securityContext);
 
         $this->_constraints = $manager->getConstraints( $validator );
     }
@@ -36,14 +36,6 @@ class ContactType extends NodevoContactType
         parent::buildForm($builder, $options);
         
         $builder
-        ->add('fonctionStructure', 'text', array(
-                'max_length' => $this->_constraints['fonctionStructure']['maxlength'],
-                'required'   => false,
-                'label'      => 'Fonction dans l\'établissement',
-                'attr'       => array(
-                        'class' => $this->_constraints['fonctionStructure']['class']
-                )
-        ))
 
         ->add('civilite', 'entity', array(
                 'class'         => 'HopitalNumeriqueReferenceBundle:Reference',
@@ -59,67 +51,7 @@ class ContactType extends NodevoContactType
                         ->orderBy('ref.order', 'ASC');
                 }
         ))
-        
-        ->add('region', 'entity', array(
-                'class'       => 'HopitalNumeriqueReferenceBundle:Reference',
-                'property'    => 'libelle',
-                'required'    => false,
-                'label'       => 'Région',
-                'empty_value' => ' - ',
-                'query_builder' => function(EntityRepository $er) {
-                    return $er->createQueryBuilder('ref')
-                    ->where('ref.code = :etat')
-                    ->setParameter('etat', 'REGION')
-                    ->orderBy('ref.order', 'ASC');
-                }
-        ))
-        
-        ->add('departement', 'entity', array(
-                'class'       => 'HopitalNumeriqueReferenceBundle:Reference',
-                'property'    => 'libelle',
-                'required'    => false,
-                'label'       => 'Département',
-                'empty_value' => ' - ',
-                'attr'        => array(),
-                'query_builder' => function(EntityRepository $er) use($options) {
-                    return $er->createQueryBuilder('ref')
-                    ->where('ref.code = :etat')
-                    ->setParameter('etat', 'DEPARTEMENT')
-                    ->orderBy('ref.libelle', 'ASC');
-                }
-        ))
 
-        ->add('statutEtablissementSante', 'entity', array(
-                'class'       => 'HopitalNumeriqueReferenceBundle:Reference',
-                'property'    => 'libelle',
-                'required'    => false,
-                'label'       => 'Type d\'établissement',
-                'empty_value' => ' - ',
-                'attr'        => array('class' => 'etablissement_sante'),
-                'query_builder' => function(EntityRepository $er) {
-                    return $er->createQueryBuilder('ref')
-                    ->where('ref.code = :etat')
-                    ->setParameter('etat', 'CONTEXTE_TYPE_ES')
-                    ->orderBy('ref.libelle', 'ASC');
-                }
-        ))
-        
-        ->add('etablissementRattachementSante', 'genemu_jqueryselect2_entity', array(
-                'class'         => 'HopitalNumeriqueEtablissementBundle:Etablissement',
-                'property'      => 'usersAffichage',
-                'multiple'      => false,
-                'required'      => false,
-                'label'         => 'Etablissement de rattachement',
-                'empty_value'   => ' - ',
-                'attr'        => array('class' => 'etablissement_sante')
-        ))
-
-        ->add('autreStructureRattachementSante', 'text', array(
-                'max_length' => $this->_constraints['autreStructureRattachementSante']['maxlength'],
-                'required'   => false,
-                'label'      => 'Nom de votre établissement si non disponible dans la liste précédente',
-                'attr'       => array('class' => $this->_constraints['autreStructureRattachementSante']['class'] . ' etablissement_sante' )
-        ))
         ;
     }
     
