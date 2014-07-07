c937e2c1f658904c07f2c4ed6c2e4fe69b4f01c3
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2014-07-07 16:11:30 +0200

    Forum : ajout des envoies de mail sur les réponses

--- script-sql/hopitalnumerique-forum.sql
+++ script-sql/hopitalnumerique-forum.sql
@@ -1,1 +1,5 @@
 /* 11:48:58 Gaia (localnodevo) */ UPDATE `core_menu_item` SET `itm_route` = 'ccdn_forum_user_category_index', `itm_uri` = '' WHERE `itm_id` = '67';
+
+INSERT INTO `core_mail` (`mail_id`, `mail_objet`, `mail_description`, `mail_expediteur_mail`, `mail_expediteur_name`, `mail_body`, `mail_params`)
+VALUES
+    (36, '[HOPITALNUMERIQUE] - Nouveau message sur le forum', 'Nouveau message sur le forum', 'communication@anap.fr', 'ANAP Hôpital numérique', 'Bonjour %u,\n\nUn nouveau message a été posté sur un forum d’Hôpital Numérique :\n- Forum : %forum\n- Catégorie : %categorie\n- Thème : %theme\n- Fil de discussion : %fildiscusssion\n- Lien vers le message : %lienversmessage\n\n\nCordialement,', '{\"%u\":\"Nom d\'utilisateur\",\"%forum\":\"Nom du forum sur lequel le nouveau message est posté.\",\"%categorie\":\"Nom de la catégorie à la laquelle le message a été posté.\",\"%fildiscusssion\":\"Titre du fil de discussion.\", \"%lienversmessage\":\"URL vers le nouveau message posté.\"}');
