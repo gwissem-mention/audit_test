aafe208234506f6028a8c6ff4b3589fd3a19dcb8
Quentin SOMAZZI <qsomazzi@gmail.com>
2014-08-07 14:24:37 +0200

    fix

---
+++ src/HopitalNumerique/AccountBundle/Resources/public/js/script.js
@@ -0,0 +1,61 @@
+var gridster;
+
+$(function() {
+    gridster = $(".gridster ul").gridster({
+        widget_margins         : [5, 10],
+        widget_base_dimensions : [410, 200],
+        max_cols               : 2,
+        min_cols               : 2,
+        serialize_params       : function($w, wgd) { return { col: wgd.col, row: wgd.row, id: $w.data('id') } },
+        draggable              : {
+            stop : function(event, ui){ 
+                $.ajax({
+                    url  : $('#account-reorder-url').val(),
+                    data : {
+                        datas : gridster.serialize()
+                    },
+                    type     : 'POST',
+                    dataType : 'json',
+                    success  : function( data ){
+                       //reorder done with success
+                    }
+                });
+            }
+        }
+    }).data('gridster');
+});
+
+enquire.register("screen and (max-width: 1196px)", {
+    match : function() {
+        $(function() {
+            gridster.destroy();
+            $(".gridster ul").css('height', 'auto').css('width', 'auto');
+        });
+    },
+    unmatch : function() {
+        $(function() {
+            gridster = $(".gridster ul").gridster({
+                widget_margins         : [5, 10],
+                widget_base_dimensions : [410, 200],
+                max_cols               : 2,
+                min_cols               : 2,
+                serialize_params       : function($w, wgd) { return { col: wgd.col, row: wgd.row, id: $w.data('id') } },
+                draggable              : {
+                    stop : function(event, ui){ 
+                        $.ajax({
+                            url  : $('#account-reorder-url').val(),
+                            data : {
+                                datas : gridster.serialize()
+                            },
+                            type     : 'POST',
+                            dataType : 'json',
+                            success  : function( data ){
+                               //reorder done with success
+                            }
+                        });
+                    }
+                }
+            }).data('gridster');
+        });
+    }
+});
