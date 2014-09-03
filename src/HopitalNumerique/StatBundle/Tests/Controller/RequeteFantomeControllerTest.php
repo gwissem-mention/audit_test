95c7faabab6eaf9c31bec7520a94de20857f621e
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2014-09-03 13:58:10 +0200

    Tableaux Stats: recherche aidée + recherches des prods

--- src/HopitalNumerique/StatBundle/Tests/Controller/RequeteFantomeControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace HopitalNumerique\StatBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class RequeteFantomeControllerTest extends WebTestCase
-{
-}
