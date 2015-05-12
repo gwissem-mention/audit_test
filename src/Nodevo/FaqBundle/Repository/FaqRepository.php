1d34330e13ac8f7c40598905821f24c70c1e35d4
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2015-05-12 14:58:09 +0200

    FAQ

--- src/Nodevo/FaqBundle/Repository/FaqRepository.php
+++ src/Nodevo/FaqBundle/Repository/FaqRepository.php
@@ -9,5 +9,26 @@ use Doctrine\ORM\EntityRepository;
  */
 class FaqRepository extends EntityRepository
 {
-
+    /**
+     * Récupère les données du grid sous forme de tableau correctement formaté
+     *
+     * @return Query Builder
+     */
+    public function getDatasForGrid($domainesIds, $condition = null)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('faq.id, faq.question, faq.reponse, faq.order, categorie.name as categorieName, domaine.nom as domaineNom')
+            ->from('NodevoFaqBundle:Faq', 'faq')
+            ->leftJoin('faq.categorie', 'categorie')
+            ->leftJoin('faq.domaines', 'domaine')
+                ->where($qb->expr()->orX(
+                    $qb->expr()->in('domaine.id', ':domainesId'),
+                    $qb->expr()->isNull('domaine.id')
+                ))
+                ->setParameter('domainesId', $domainesIds)
+            // ->groupBy('faq')
+            ->orderBy('faq.order');
+            
+        return $qb;
+    }
 }
