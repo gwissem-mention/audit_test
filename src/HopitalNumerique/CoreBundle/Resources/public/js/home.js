f558cdc521859c0c404169c267b9ac35f1a190ac
Gaetan MELCHILSEN <gaetan.melchilsen@gmail.com>
2015-06-24 10:16:22 +0200

    Modifs en cours de la home

--- src/HopitalNumerique/CoreBundle/Resources/public/js/home.js
+++ src/HopitalNumerique/CoreBundle/Resources/public/js/home.js
@@ -25,9 +25,6 @@ $(document).ready(function(){
             }
     );
 
-
-    $("#menu-container").find('ul.menu_level_1').addClass('menu-bottom');
-
     $('.onbottomdown').click(function() {
         $(this).hide();
         $('.onbottomup').show();
@@ -37,6 +34,19 @@ $(document).ready(function(){
         $(this).hide();
         $('.onbottomdown').show();
     });
+
+    
+    //Cache le bloc de références
+    $('#redirection-references').hide();
+    //Cache toutes les questions
+    $('#expression-du-besoin .expression-du-besoin-form').each(function(){
+        $(this).hide();
+    });
+
+    $('#question-precedente').hide();
+
+    //Ré-Affiche la première question (l'ordre le plus bas)
+    $('#expression-du-besoin .expression-du-besoin-form').first().show();
 
     $('#block-carte-france').hover(
         function() {
@@ -48,19 +58,95 @@ $(document).ready(function(){
     )
 });
 
-$(window).scroll(function() {
-    var scrollTop = $(this).scrollTop();
-    if(scrollTop <= 100 ) {
-        $("#menu-container").find('ul.menu_level_1').addClass('menu-bottom');
-    } else {
-        $("#menu-container").find('ul.menu_level_1').removeClass('menu-bottom');
-    }
+function retourDerniereQuestion(idQuestion)
+{
+    //Création du loader
+    var loader = $('#expression-du-besoin').nodevoLoader().start();
 
-    if($(window).width() > 1650) {
-        if(scrollTop >= ($(window).height() - $('#menu-container').outerHeight())) {
-            $("#menu-container").addClass('menu-sticky');
-        } else {
-            $("#menu-container").removeClass('menu-sticky');
-        }
+    var arrayClics = $.parseJSON($("#order-clic").val());
+    $('#expression-du-besoin').find('#expBesoin-' + arrayClics[0]).show();
+    $('#expression-du-besoin').find('#expBesoin-' + idQuestion).hide();
+    
+    arrayClics.shift();
+
+    if(arrayClics.length === 0)
+        $('#question-precedente').hide();
+
+    //Mise à jour de l'input hidden
+    $("#order-clic").val(JSON.stringify(arrayClics));
+    loader.finished();
+}
+
+function clickReponse( idReponse, idQuestion )
+{
+    var loader = $('#expression-du-besoin').nodevoLoader().start();
+
+    //Sauvegarde du clic sur la réponse en base
+    sauvegardeClicStat(idReponse);
+
+    //Récupération des réponses
+    var reponses = $.parseJSON($('#reponses-json').val());
+
+    //Récupération de la réponse courante
+    var reponsesCourante = reponses[idReponse];
+
+    //Cache la question courante
+    //$(this).parents('.expression-du-besoin-form').hide();
+    $('#expression-du-besoin').find('#expBesoin-' + idQuestion).hide();
+
+    //Vérifie si on pointe sur une nouvelle question ou si on doit rediriger vers la recherche
+    if(reponsesCourante['autreQuestion'])
+    {
+        //Affiche celle sur laquelle la réponse pointe
+        $('#expression-du-besoin').find('#expBesoin-' + reponsesCourante['idQuestion']).show();
+
+        var arrayClics = $.parseJSON($("#order-clic").val());
+        arrayClics.unshift(idQuestion);
+        //Mise à jour de l'input hidden
+        $("#order-clic").val(JSON.stringify(arrayClics));
+        $('#question-precedente').show();
+
+        loader.finished();
     }
-});
+    else
+    {
+        var path   = $('#url-modification-session-recherche').val();
+
+        //Génération du cookie JS pour la recherche
+        $.ajax({
+            url      : path,
+            data : {
+                id : idReponse
+            },
+            type     : 'POST',
+            dataType : 'json',
+            success : function( data ){
+                if( data.success )
+                {
+                    parent.location = $("#url-recherche").val();                    
+                }
+                else
+                {
+                    apprise('Une erreur est survenue lors du chargement du calcul de votre recherche, merci de réessayer.');
+                    loader.finished();
+                }
+            }
+        });
+    }
+}
+
+function sauvegardeClicStat( idReponse )
+{
+    var path   = $('#url-sauvegarde-clic-stat').val();
+
+    $.ajax({
+        url      : path,
+        data : {
+            id : idReponse
+        },
+        type     : 'POST',
+        dataType : 'json',
+        success : function( data ){
+        }
+    });
+}
