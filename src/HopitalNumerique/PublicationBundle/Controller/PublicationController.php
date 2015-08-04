6b4b47ca89fa49a4927651ef1c280260bf93558f
Gaetan Melchilsen <gmelchilsen@nodevo.com>
2015-08-04 08:26:18 +0200

    Correctif apc

--- src/HopitalNumerique/PublicationBundle/Controller/PublicationController.php
+++ src/HopitalNumerique/PublicationBundle/Controller/PublicationController.php
@@ -52,7 +52,7 @@ class PublicationController extends Controller
         //~~APC~~
         $cacheDriver = new ApcCache();
         $cacheName = "_publication_objet_" . $objet->getId();
-        if (false)//$cacheDriver->contains($cacheName))
+        if ($cacheDriver->contains($cacheName))
         {
             $cache = $cacheDriver->fetch($cacheName);
 
