e86dc704ab57a3e8ee61e6555312d8d957971acd
Gaetan Melchilsen <gmelchilsen@nodevo.com>
2015-09-04 10:05:43 +0200

    3065

--- src/HopitalNumerique/QuestionnaireBundle/Grid/QuestionnaireGrid.php
+++ src/HopitalNumerique/QuestionnaireBundle/Grid/QuestionnaireGrid.php
@@ -53,14 +53,6 @@ class QuestionnaireGrid extends Grid implements GridInterface
         $listQuestionsButton->setRouteParameters( array('id') );
         $listQuestionsButton->setAttributes( array('class'=>'btn btn-success fa fa-list','title' => 'Lister les questions') );
         $this->addActionButton( $listQuestionsButton );
-
-        $viderReponsesButton = new Action\ShowButton( 'hopitalnumerique_reponse_deleteAllByQuestionnaire' );
-        $viderReponsesButton->setRouteParameters( array('id') );
-        $viderReponsesButton->setAttributes( array(
-                'class'=>'btn btn-primary fa fa-trash-o',
-                'title' => 'Vider l\'ensemble des réponses du questionnaire.'
-        ));
-        $this->addActionButton( $viderReponsesButton );
     }
 
     /**
