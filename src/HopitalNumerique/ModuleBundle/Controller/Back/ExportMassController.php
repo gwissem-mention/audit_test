b994b9f7aaec35309889730788317f7861984a2a
Gaetan Melchilsen <gmelchilsen@nodevo.com>
2015-09-17 08:57:25 +0200

    Expert grid

--- src/HopitalNumerique/ModuleBundle/Controller/Back/ExportMassController.php
+++ src/HopitalNumerique/ModuleBundle/Controller/Back/ExportMassController.php
@@ -26,6 +26,43 @@ class ExportMassController extends Controller
             foreach($rawDatas as $data)
                 $primaryKeys[] = $data['id'];
         }
+
+        $inscriptions = $this->get('hopitalnumerique_module.manager.inscription')->findBy( array('id' => $primaryKeys) );
+
+        $colonnes = array( 
+                            'id'                        => 'id', 
+                            'user.nom'                  => 'Nom', 
+                            'user.prenom'               => 'Prénom', 
+                            'user.username'             => 'Identifiant (login)', 
+                            'user.email'                => 'Adresse e-mail',
+                            'session.moduleTitre'       => 'Titre du module',
+                            'session.dateSessionString' => 'Début de la session',
+                            'etatInscription.libelle'   => 'Inscription',
+                            'etatParticipation.libelle' => 'Participation',
+                            'etatEvaluation.libelle'    => 'Evaluation',
+                            'dateInscriptionString'     => 'Date d\'inscription'
+                        );
+
+        $kernelCharset = $this->container->getParameter('kernel.charset');
+
+        return $this->get('hopitalnumerique_module.manager.inscription')->exportCsv( $colonnes, $inscriptions, 'export-utilisateurs.csv', $kernelCharset );
+    }
+
+    /**
+     * Export CSV de la liste des sessions 
+     *
+     * @param array $primaryKeys    ID des lignes sélectionnées
+     * @param array $allPrimaryKeys allPrimaryKeys ???
+     */
+    public function exportAllCsvAction( $primaryKeys, $allPrimaryKeys )
+    {
+        //get all selected inscription
+        if($allPrimaryKeys == 1){
+            $rawDatas = $this->get('hopitalnumerique_module.grid.allinscription')->getRawData();
+            foreach($rawDatas as $data)
+                $primaryKeys[] = $data['id'];
+        }
+
         $inscriptions = $this->get('hopitalnumerique_module.manager.inscription')->findBy( array('id' => $primaryKeys) );
 
         $colonnes = array( 
