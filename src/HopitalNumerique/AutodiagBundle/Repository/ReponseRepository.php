c02a02b58ac750f73223f0408d46d57777f2da79
Gaetan Melchilsen <gmelchilsen@nodevo.com>
2015-10-20 10:04:18 +0200

    Opti export excel resultat autodiag

--- src/HopitalNumerique/AutodiagBundle/Repository/ReponseRepository.php
+++ src/HopitalNumerique/AutodiagBundle/Repository/ReponseRepository.php
@@ -9,5 +9,22 @@ use Doctrine\ORM\EntityRepository;
  */
 class ReponseRepository extends EntityRepository
 {
-    
+    /**
+     * Retourne la liste des questions dans le bon ordre
+     *
+     * @param Chapitre $chapitre chapitre
+     *
+     * @return integer
+     */
+    public function getReponsesByResultats( $resultatIds )
+    {
+        return $this->_em->createQueryBuilder()
+                         ->select('rep.id, rep.value, rep.remarque, res.id as resId, que.id as queId')
+                         ->from('\HopitalNumerique\AutodiagBundle\Entity\Reponse', 'rep')
+                         ->leftJoin('rep.question', 'que')
+                         ->leftJoin('rep.resultat', 'res')
+                         ->andWhere('res.id IN (:resultatIds)')
+                         ->setParameter('resultatIds', $resultatIds )
+                         ->orderBy('res.id', 'ASC');
+    }
 }
