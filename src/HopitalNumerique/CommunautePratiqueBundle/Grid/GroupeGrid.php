6e22f75cf4c3c9fbfdf9143e441f00332fa0f306
Rémi Leclerc <rleclerc@nodevo.com>
2015-11-06 11:54:17 +0100

    Init Manager Groupe

--- src/HopitalNumerique/CommunautePratiqueBundle/Grid/GroupeGrid.php
+++ src/HopitalNumerique/CommunautePratiqueBundle/Grid/GroupeGrid.php
@@ -1,8 +1,38 @@
 <?php
+namespace HopitalNumerique\CommunautePratiqueBundle\Grid;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Grid de Groupe.
  */
-
+class GroupeGrid extends \Nodevo\GridBundle\Grid\Grid
+{
+    /**
+     * @var \HopitalNumerique\UserBundle\Entity\User Utilisateur connecté
+     */
+    private $user;
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function __construct(\Symfony\Component\DependencyInjection\ContainerInterface $container)
+    {
+        parent::__construct($container);
+        
+        $this->user = $container->get('security.context')->getToken()->getUser();
+        if (!($this->user instanceof HopitalNumerique\UserBundle\Entity\User))
+        {
+            throw new \Exception('Aucun utilisateur connecté.');
+        }
+    }
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function setConfig()
+    {
+        $this->setSource('hopitalnumerique_communautepratique.manager.groupe');
+        $this->setSourceType(self::SOURCE_TYPE_MANAGER);
+    }
+}
---
+++ src/HopitalNumerique/CommunautePratiqueBundle/Manager/GroupeManager.php
@@ -0,0 +1,9 @@
+<?php
+namespace HopitalNumerique\CommunautePratiqueBundle\Grid;
+
+class GroupeManager extends \Nodevo\ToolsBundle\Manager\Manager
+{
+    protected $_class = 'HopitalNumerique\GroupeManagerBundle\Entity\Groupe';
+    
+    
+}
