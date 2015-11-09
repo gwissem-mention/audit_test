e28321d9d37eafc35b411ab01ccdd3ab86ed2788
Thomas DAURIAC <tdauriac@nodevo.com>
2015-11-09 13:54:44 +0000

    SearchManager.php edited online with Bitbucket

--- src/HopitalNumerique/RechercheBundle/Manager/SearchManager.php
+++ src/HopitalNumerique/RechercheBundle/Manager/SearchManager.php
@@ -881,7 +881,7 @@ class SearchManager extends BaseManager
 
         $item['id']       = $topic->getId();
         $item['titre']    = $topic->getTitle();
-        $item['titre']    = "test";
+        $item['resume']   = $topic->getFirstPost()->getBody();
         $item['countRef'] = $this->getNoteReferencement($topic->getReferences());
         
         //get Type
