22d398b73fd002aca2c22a00a9e8b21d335922a8
Rémi Leclerc <rleclerc@nodevo.com>
2015-12-08 15:49:24 +0100

    TDB : Forums

--- src/HopitalNumerique/CommunautePratiqueBundle/Controller/AccueilController.php
+++ src/HopitalNumerique/CommunautePratiqueBundle/Controller/AccueilController.php
@@ -3,6 +3,7 @@ namespace HopitalNumerique\CommunautePratiqueBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use HopitalNumerique\ReferenceBundle\Entity\Reference;
+use HopitalNumerique\ForumBundle\Entity\Forum;
 
 /**
  * Accueil de la communauté de pratiques.
@@ -14,20 +15,31 @@ class AccueilController extends \Symfony\Bundle\FrameworkBundle\Controller\Contr
      */
     public function indexAction(Request $request)
     {
-        if (!$this->container->get('hopitalnumerique_communautepratique.dependency_injection.security')->canAccessCommunautePratique()) {
+        if (!$this->container->get('hopitalnumerique_communautepratique.dependency_injection.security')
+            ->canAccessCommunautePratique()) {
             return $this->redirect($this->generateUrl('hopital_numerique_homepage'));
         }
 
-        $domaine = $this->container->get('hopitalnumerique_domaine.manager.domaine')->findOneById($request->getSession()->get('domaineId'));
+        $domaine = $this->container->get('hopitalnumerique_domaine.manager.domaine')
+            ->findOneById($request->getSession()->get('domaineId'));
+
+        $forum = $this->container->get('hopitalnumerique_forum.manager.forum')
+            ->findOneById(Forum::FORUM_COMMUNAUTE_DE_PRATIQUES_ID);
+        $forumLastTopics = $this->container->get('hopitalnumerique_forum.manager.topic')
+            ->getLastTopicsForum($forum->getId(), 3);
 
         return $this->render(
             'HopitalNumeriqueCommunautePratiqueBundle:Accueil:index.html.twig',
             array(
-                'groupesEnVedette' => $this->container->get('hopitalnumerique_communautepratique.manager.groupe')->findNonFermes($domaine, true),
-                'userGroupesEnCours' => $this->container->get('hopitalnumerique_communautepratique.manager.groupe')->findEnCoursByUser($domaine, $this->getUser()),
+                'groupesEnVedette' => $this->container->get('hopitalnumerique_communautepratique.manager.groupe')
+                    ->findNonFermes($domaine, true),
+                'userGroupesEnCours' => $this->container->get('hopitalnumerique_communautepratique.manager.groupe')
+                    ->findEnCoursByUser($domaine, $this->getUser()),
                 'totalMembres' => $this->container->get('hopitalnumerique_user.manager.user')
                     ->findCommunautePratiqueMembresCount(),
-                'membres' => $this->getMembresAuHasard()
+                'membres' => $this->getMembresAuHasard(),
+                'forum' => $forum,
+                'forumLastTopics' => $forumLastTopics
             )
         );
     }
