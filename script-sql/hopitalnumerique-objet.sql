6092dee6f5074f291046b9db6520bca4a81c0ed4
Thomas DAURIAC <tdauriac@nodevo.com>
2015-12-23 09:08:08 +0100

    script

--- script-sql/hopitalnumerique-objet.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE hn_objet ADD cp_group_id INT UNSIGNED DEFAULT NULL;
-ALTER TABLE hn_objet ADD CONSTRAINT FK_C09252D862654CC4 FOREIGN KEY (cp_group_id) REFERENCES hn_communautepratique_groupe (group_id);
-CREATE INDEX IDX_C09252D862654CC4 ON hn_objet (cp_group_id);
