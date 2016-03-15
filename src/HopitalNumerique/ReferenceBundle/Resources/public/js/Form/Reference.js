3a7db76df728c12d4f1d995fe4dd51a38677e888
Rémi Leclerc <rleclerc@nodevo.com>
2016-03-15 11:10:10 +0100

    description courte oblig si dans glossaire

--- src/HopitalNumerique/ReferenceBundle/Resources/public/js/Form/Reference.js
+++ src/HopitalNumerique/ReferenceBundle/Resources/public/js/Form/Reference.js
@@ -18,6 +18,7 @@ Hn_Reference_Form_Reference.init = function()
 {
     Hn_Reference_Form_Reference.initEvents();
     Hn_Reference_Form_Reference.processInRechercheDisplaying();
+    Hn_Reference_Form_Reference.processDescriptionCourteObligatory();
 };
 
 /**
@@ -27,6 +28,9 @@ Hn_Reference_Form_Reference.initEvents = function()
 {
     $('#hopitalnumerique_reference_reference_reference').click(function() {
         Hn_Reference_Form_Reference.processInRechercheDisplaying();
+    });
+    $('#hopitalnumerique_reference_reference_inGlossaire').click(function() {
+        Hn_Reference_Form_Reference.processDescriptionCourteObligatory();
     });
 };
 
@@ -41,4 +45,18 @@ Hn_Reference_Form_Reference.processInRechercheDisplaying = function()
         $('#hopitalnumerique_reference_reference_inRecherche').prop('checked', false);
     }
     $('#reference-in-recherche-container').css({ display:(isReference ? 'block' : 'none') });
+};
+
+/**
+ * Rend la description courte obligatoire si le concept est présent dans le glossaire.
+ */
+Hn_Reference_Form_Reference.processDescriptionCourteObligatory = function()
+{
+    var isInGlossaire = $('#hopitalnumerique_reference_reference_inGlossaire').is(':checked');
+
+    if (isInGlossaire) {
+        $('#hopitalnumerique_reference_reference_descriptionCourte').addClass('validate[required]');
+    } else {
+        $('#hopitalnumerique_reference_reference_descriptionCourte').removeClass('validate[required]');
+    }
 };
