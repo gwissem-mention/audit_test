4bad84e0e08e08a10e760f02ed00816f71b510ad
Rémi Leclerc <rleclerc@nodevo.com>
2016-04-07 18:04:27 +0200

    Glossaire suppr delete

--- src/HopitalNumerique/GlossaireBundle/Controller/GlossaireController.php
+++ src/HopitalNumerique/GlossaireBundle/Controller/GlossaireController.php
@@ -21,24 +21,6 @@ class GlossaireController extends Controller
     }
 
     /**
-     * Suppresion d'un Glossaire.
-     * 
-     * @param integer $id Id de Glossaire.
-     * METHOD = POST|DELETE
-     */
-    public function deleteAction( $id )
-    {
-        $glossaire = $this->get('hopitalnumerique_glossaire.manager.glossaire')->findOneBy( array( 'id' => $id) );
-
-        //Suppression de l'entitée
-        $this->get('hopitalnumerique_glossaire.manager.glossaire')->delete( $glossaire );
-
-        $this->get('session')->getFlashBag()->add('info', 'Suppression effectuée avec succès.' );
-
-        return new Response('{"success":true, "url" : "'.$this->generateUrl('hopitalnumerique_glossaire_glossaire').'"}', 200);
-    }
-
-    /**
      * Parse les publication à la recherche de mots du glossaire
      *
      * @return redirect
