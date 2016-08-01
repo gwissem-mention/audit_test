3cf5a7e9a06843de4a41e29a09581232f0a20722
Emmanuel Da Fonseca <edafonseca@nodevo.com>
2016-08-01 09:45:37 +0200

    Ajout du gabarit

--- src/HopitalNumerique/AutodiagBundle/Controller/Back/ExportController.php
+++ src/HopitalNumerique/AutodiagBundle/Controller/Back/ExportController.php
@@ -13,4 +13,11 @@ class ExportController extends Controller
         $exporter = new SurveyExport($this->getDoctrine()->getManager());
         return $exporter->export($autodiag);
     }
+
+    public function surveyLayoutAction()
+    {
+        $fileName =  $this->getParameter("kernel.root_dir") . '/../files/autodiag/questionnaire.xlsx';
+
+        return $this->get('igorw_file_serve.response_factory')->create($fileName);
+    }
 }
