dd867d510a70bb6b28e80f0824cda2e6afefb0aa
Emmanuel Da Fonseca <edafonseca@nodevo.com>
2016-08-01 15:17:13 +0200

    Suppression debug

--- src/HopitalNumerique/AutodiagBundle/Controller/Back/AutodiagController.php
+++ src/HopitalNumerique/AutodiagBundle/Controller/Back/AutodiagController.php
@@ -108,7 +108,6 @@ class AutodiagController extends Controller
                 $this->addFlash('success', $this->get('translator')->trans('ad.import.success'));
             } catch (\Exception $e) {
                 $this->addFlash('danger', $this->get('translator')->trans('ad.import.errors.generic'));
-                dump($e);die;
             }
             return $this->redirectToRoute('hopitalnumerique_autodiag_edit_survey', [
                 'id' => $autodiag->getId()
