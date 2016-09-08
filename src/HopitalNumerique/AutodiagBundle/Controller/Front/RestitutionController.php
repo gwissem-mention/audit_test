4d3291f939223aa976cccb65f2ad9c9a43c3924c
Emmanuel Da Fonseca <edafonseca@nodevo.com>
2016-09-08 11:17:37 +0200

    Export PDF

--- src/HopitalNumerique/AutodiagBundle/Controller/Front/RestitutionController.php
+++ src/HopitalNumerique/AutodiagBundle/Controller/Front/RestitutionController.php
@@ -3,10 +3,11 @@
 namespace HopitalNumerique\AutodiagBundle\Controller\Front;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class RestitutionController extends Controller
 {
-    public function indexAction($synthesis)
+    public function indexAction($synthesis, $pdf = false)
     {
         $synthesis = $this->getDoctrine()->getManager()->getRepository('HopitalNumeriqueAutodiagBundle:Synthesis')
             ->getFullyLoadedSynthesis($synthesis);
@@ -38,6 +39,38 @@ class RestitutionController extends Controller
 
         $calculator = $this->get('autodiag.restitution.calculator');
         $resultItems = $calculator->compute($synthesis);
+
+        if ($pdf) {
+            $html = $this->renderView('HopitalNumeriqueAutodiagBundle:Restitution:pdf.html.twig', [
+                'synthesis' => $synthesis,
+                'restitution' => $restitution,
+                'result' => $resultItems,
+            ]);
+
+            $options = array(
+                'encoding'         => 'UTF-8',
+                'javascript-delay' => 1000,
+                'margin-top'       => '15',
+                'margin-bottom'    => '25',
+                'margin-right'     => '15',
+                'margin-left'      => '15',
+                'header-spacing'   => '2',
+                'header-left'      => date('d/m/Y'),
+                'header-right'     => 'Page [page] / [toPage]',
+                'header-font-size' => '10',
+                'footer-spacing'   => '10',
+                'page-width' => '1024px',
+                'footer-html'      => '<p style="font-size:10px;text-align:center;color:#999"> &copy; ANAP<br>Ces contenus extraits de l\'ANAP sont diffus&eacute;s gratuitement.<br>Toutefois, leur utilisation ou citation est soumise &agrave; l\'inscription de la mention suivante : "&copy; ANAP"</p>'
+            );
+
+            return new Response(
+                $this->get('knp_snappy.pdf')->getOutputFromHtml($html, $options, true),
+                200,
+                array(
+                    'Content-Type'        => 'application/pdf',
+                )
+            );
+        }
 
         return $this->render('HopitalNumeriqueAutodiagBundle:Restitution:index.html.twig', [
             'synthesis' => $synthesis,
