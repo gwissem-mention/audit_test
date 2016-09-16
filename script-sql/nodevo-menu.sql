082571cc380b23a8b1afd8fc9020be5afe947326
Thomas DAURIAC <tdauriac@nodevo.com>
2016-09-16 11:58:54 +0200

    SQL

--- script-sql/nodevo-menu.sql
+++
@@ -1,5 +0,0 @@
--- Ajouter le menu Mes autodiagnostics dans Mon compte FO
-
-INSERT INTO `core_menu_item` (`itm_id`, `itm_parent`, `mnu_menu`, `itm_name`, `itm_route`, `itm_route_parameters`, `itm_route_absolute`, `itm_uri`, `itm_icon`, `itm_display`, `itm_display_children`, `itm_role`, `itm_order`)
-VALUES
-	(336, 68, 3, 'Mes autodiagnostics', 'hopitalnumerique_autodiag_account_index', NULL, 0, NULL, NULL, 1, 0, 'IS_AUTHENTICATED_ANONYMOUSLY', 4);
