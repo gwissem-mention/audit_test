47d00d85f0c3a9650c3ac869511790f15f2bd1ec
Quentin Cunat <quentin.cunat@gmail.com>
2016-11-02 15:16:09 +0100

    add topicRecommandationAction

--- src/Nodevo/MailBundle/Controller/RecommandationController.php
+++ src/Nodevo/MailBundle/Controller/RecommandationController.php
@@ -47,4 +47,38 @@ class RecommandationController extends Controller
             ]
         );
     }
+
+    public function topicRecommandationAction(Request $request)
+    {
+        $recommandationMail = $this->container->get('nodevo_mail.manager.mail')->findOneById(Mail::MAIL_RECOMMANDATION_TOPIC_ID);
+        if (null === $recommandationMail) {
+            throw new \Exception('Mail de recommandation à un ami inexistant.');
+        }
+
+        $recommandationForm = $this->createForm(RecommandationType::class, null, [
+            'mail' => $recommandationMail,
+            'expediteur' => $this->getUser(),
+            'url' => $request->headers->get('referer')
+        ]);
+        $recommandationForm->handleRequest($request);
+
+        if ($recommandationForm->isSubmitted()) {
+            $recommandationMessage = $this->container->get('nodevo_mail.manager.mail')->sendMail(
+                $recommandationForm->get('objet')->getData(),
+                $recommandationForm->get('expediteur')->getData(),
+                $recommandationForm->get('destinataire')->getData(),
+                $recommandationForm->get('message')->getData()
+            );
+            $this->container->get('mailer')->send($recommandationMessage);
+            $this->addFlash('success', 'Recommandation envoyée.');
+            return $this->redirect($recommandationForm->get('url')->getData());
+        }
+
+        return $this->render(
+            'NodevoMailBundle:Recommandation:popin.html.twig',
+            [
+                'recommandationForm' => $recommandationForm->createView()
+            ]
+        );
+    }
 }
