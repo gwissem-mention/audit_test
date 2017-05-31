043b6578c8f51ec917640aeaa5e76804756cc20c
7487_webmaster <7487_webmaster@ds-195-210.produhost.net>
2017-05-31 09:04:22 +0200

    initialise domains slugs

---
+++ script-sql/nodevo-search.sql
@@ -0,0 +1,15 @@
+/* Initialise domains slugs */
+UPDATE `hn_domaine` SET `dom_slug` = 'numerique' WHERE `dom_id` = '1';
+UPDATE `hn_domaine` SET `dom_slug` = 'macrodiag' WHERE `dom_id` = '2';
+UPDATE `hn_domaine` SET `dom_slug` = 'transports-sanitaires' WHERE `dom_id` = '3';
+UPDATE `hn_domaine` SET `dom_slug` = 'psychiatrie-sante-mentale' WHERE `dom_id` = '4';
+UPDATE `hn_domaine` SET `dom_slug` = 'formations-plan-triennal' WHERE `dom_id` = '5';
+UPDATE `hn_domaine` SET `dom_slug` = 'rse' WHERE `dom_id` = '7';
+UPDATE `hn_domaine` SET `dom_slug` = 'bloc-operatoire\'' WHERE `dom_id` = '8';
+UPDATE `hn_domaine` SET `dom_slug` = 'pf-test' WHERE `dom_id` = '9';
+UPDATE `hn_domaine` SET `dom_slug` = 'medicaments' WHERE `dom_id` = '10';
+UPDATE `hn_domaine` SET `dom_slug` = 'gestion-des-lits\'' WHERE `dom_id` = '11';
+UPDATE `hn_domaine` SET `dom_slug` = 'immobilier' WHERE `dom_id` = '12';
+UPDATE `hn_domaine` SET `dom_slug` = 'imagerie' WHERE `dom_id` = '13';
+UPDATE `hn_domaine` SET `dom_slug` = 'urgences' WHERE `dom_id` = '14';
+UPDATE `hn_domaine` SET `dom_slug` = 'si-ght' WHERE `dom_id` = '15';
