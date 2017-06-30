8c8eb10705af63ca9210641dceb7dadbfec9eff8
Guillaume <gv.vermeersch@gmail.com>
2017-06-30 11:09:06 +0200

    use right function

--- src/HopitalNumerique/ForumBundle/EventListener/PostEventListener.php
+++ src/HopitalNumerique/ForumBundle/EventListener/PostEventListener.php
@@ -192,8 +192,8 @@ class PostEventListener implements EventSubscriberInterface
                             'theme'           => $topic->getBoard()->getName(),
                             'fildiscusssion'  => $topic->getTitle(),
                             'lienversmessage' => 'lien',
-                            'pseudouser'      => !is_null($user->getPseudonymeForum())
-                                ? $user->getPseudonymeForum()
+                            'pseudouser'      => !is_null($user->getPseudonym())
+                                ? $user->getPseudonym()
                                 : $user->getNomPrenom(),
                             'shortMessage'    => $post->getBody(),
                         ];
