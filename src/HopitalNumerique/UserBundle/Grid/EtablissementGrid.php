fa2760a6241893d9e873b3b7651a322f0a96a23e
Grégoire Rondet <rondetgregoire@gmail.com>
2017-07-04 11:07:21 +0200

    7209 - Addition of domain names for other structures

--- src/HopitalNumerique/UserBundle/Grid/EtablissementGrid.php
+++ src/HopitalNumerique/UserBundle/Grid/EtablissementGrid.php
@@ -2,10 +2,12 @@
 
 namespace HopitalNumerique\UserBundle\Grid;
 
+use APY\DataGridBundle\Grid\Row;
 use Nodevo\GridBundle\Grid\Grid;
-use Nodevo\GridBundle\Grid\GridInterface;
 use Nodevo\GridBundle\Grid\Column;
 use Nodevo\GridBundle\Grid\Action;
+use Nodevo\GridBundle\Grid\GridInterface;
+use APY\DataGridBundle\Grid\Action\RowAction;
 
 /**
  * Configuration du Grid User.
@@ -43,6 +45,8 @@ class EtablissementGrid extends Grid implements GridInterface
 
         $this->addColonne(new Column\TextColumn('autreStructureRattachementSante', 'Etablissement autre'));
 
+        $this->addColonne(new Column\TextColumn('domainName', 'Domaine(s)'));
+
         $archiverColonne = new Column\BooleanColumn('archiver', 'Archivé');
         $archiverColonne->setSize(100);
         $this->addColonne($archiverColonne);
@@ -57,9 +61,9 @@ class EtablissementGrid extends Grid implements GridInterface
         $this->addActionButton(new Action\EditButton('hopital_numerique_user_edit'));
 
         //Custom Archive button : Affiche le bouton archiver
-        $archiveButton = new \APY\DataGridBundle\Grid\Action\RowAction('', 'hopitalnumerique_etablissement_archiver');
+        $archiveButton = new RowAction('', 'hopitalnumerique_etablissement_archiver');
         $archiveButton->setRouteParameters(['id']);
-        $archiveButton->manipulateRender(function (\APY\DataGridBundle\Grid\Action\RowAction $action, \APY\DataGridBundle\Grid\Row $row) {
+        $archiveButton->manipulateRender(function (RowAction $action, Row $row) {
             if (!$row->getField('archiver')) {
                 $action->setAttributes(['class' => 'btn btn-warning fa fa-archive', 'title' => 'Archiver']);
             } else {
