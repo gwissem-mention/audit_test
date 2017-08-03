e540bec6b514065aff908fa3e10fc782d75d3f55
Nicolas Pineau <npineau@nodevo.com>
2017-08-03 14:00:10 +0200

    Removed right purple box 'my search' and related elements

--- src/HopitalNumerique/RechercheBundle/Controller/ConsultationController.php
+++ src/HopitalNumerique/RechercheBundle/Controller/ConsultationController.php
@@ -5,7 +5,6 @@ namespace HopitalNumerique\RechercheBundle\Controller;
 use HopitalNumerique\ObjetBundle\Entity\Consultation;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
@@ -14,27 +13,6 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
  */
 class ConsultationController extends Controller
 {
-    /**
-     * Delete d'une consultation (AJAX).
-     *
-     * @param int $id ID de la consultation à supprimer
-     *
-     * @return Response
-     */
-    public function ajaxDeleteAction($id)
-    {
-        $consultation = $this->get('hopitalnumerique_objet.manager.consultation')->findOneBy(['id' => $id]);
-
-        //Suppression de l'entitée
-        $this->get('hopitalnumerique_objet.manager.consultation')->delete($consultation);
-        $this->get('session')->getFlashBag()->add('info', 'Suppression effectuée avec succès.');
-
-        return new Response(
-            '{"success":true, "url" : "' . $this->generateUrl('hopital_numerique_requete_homepage') . '"}',
-            200
-        );
-    }
-
     /**
      * @param Request      $request
      * @param Consultation $consultation
