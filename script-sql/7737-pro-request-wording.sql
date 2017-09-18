f7b6a9fbfece71bc7ec71e08e0199940b8259858
7487_webmaster <7487_webmaster@ds-195-210.produhost.net>
2017-09-18 17:13:00 +0200

    delete sql

--- script-sql/7737-pro-request-wording.sql
+++
@@ -1,2 +0,0 @@
-UPDATE hn_questionnaire_question SET que_libelle = "Estimez-vous utile d'avoir sollicité ce professionnel ?" WHERE que_id = 31;
-UPDATE hn_questionnaire_question SET que_libelle = "Avez-vous utilisé le Centre de ressources de l'ANAP avant l'intervention de ce professionnel ?" WHERE que_id = 29;
