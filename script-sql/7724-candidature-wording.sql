34487c4a9d27817c2c970778cefdfd2f8bc24456
Guillaume <gv.vermeersch@gmail.com>
2017-09-18 14:53:27 +0200

    Expert wording

--- script-sql/7724-candidature-wording.sql
+++ script-sql/7724-candidature-wording.sql
@@ -7,3 +7,9 @@ INSERT INTO hn_questionnaire_question (qst_id, typ_question, que_libelle, que_ob
 
 
 /** Expert **/
+UPDATE hn_questionnaire_question SET que_libelle = "Parlez-nous de votre expertise reconnue et démontrable dans les projets numériques en santé" WHERE que_id = 2;
+UPDATE hn_questionnaire_question SET que_libelle = "Pouvez-vous nous présenter en quelques phrases votre vision du développement de l'usage du numérique en santé ?" WHERE que_id = 12;
+UPDATE hn_questionnaire_question SET que_libelle = "Quelles sont pour vous les principales difficultés que les structures vont rencontrer dans l'usage du numérique en santé ?" WHERE que_id = 13;
+DELETE FROM hn_questionnaire_question WHERE que_id BETWEEN 5 AND 11;
+INSERT INTO hn_questionnaire_question (qst_id, typ_question, que_libelle, que_obligatoire, que_ordre, que_alias, que_reference_param_tri) VALUE (1, 10, "Sur quels domaines avez-vous le plus de connaissances ?", 0, 5, "fonction", "FONCTION");
+INSERT INTO hn_questionnaire_question (qst_id, typ_question, que_libelle, que_obligatoire, que_ordre, que_alias) VALUE (1, 2, "Pouvez-vous nous présenter en quelques phrases votre vision du développement de l'usage du numérique en santé ?", 0, 6, "fonction_description");
