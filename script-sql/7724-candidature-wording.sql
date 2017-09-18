c64fc9589a079acc8608740ae634b62d3471f8e3
Guillaume <gv.vermeersch@gmail.com>
2017-09-18 15:35:34 +0200

    Fix code name

--- script-sql/7724-candidature-wording.sql
+++ script-sql/7724-candidature-wording.sql
@@ -11,5 +11,8 @@ UPDATE hn_questionnaire_question SET que_libelle = "Parlez-nous de votre experti
 UPDATE hn_questionnaire_question SET que_libelle = "Pouvez-vous nous présenter en quelques phrases votre vision du développement de l'usage du numérique en santé ?" WHERE que_id = 12;
 UPDATE hn_questionnaire_question SET que_libelle = "Quelles sont pour vous les principales difficultés que les structures vont rencontrer dans l'usage du numérique en santé ?" WHERE que_id = 13;
 DELETE FROM hn_questionnaire_question WHERE que_id BETWEEN 5 AND 11;
-INSERT INTO hn_questionnaire_question (qst_id, typ_question, que_libelle, que_obligatoire, que_ordre, que_alias, que_reference_param_tri) VALUE (1, 10, "Sur quels domaines avez-vous le plus de connaissances ?", 0, 5, "fonction", "FONCTION");
+INSERT INTO hn_questionnaire_question (qst_id, typ_question, que_libelle, que_obligatoire, que_ordre, que_alias, que_reference_param_tri) VALUE (1, 10, "Sur quels domaines avez-vous le plus de connaissances ?", 0, 5, "fonction", "FONCTION_SI");
 INSERT INTO hn_questionnaire_question (qst_id, typ_question, que_libelle, que_obligatoire, que_ordre, que_alias) VALUE (1, 2, "Pouvez-vous nous présenter en quelques phrases votre vision du développement de l'usage du numérique en santé ?", 0, 6, "fonction_description");
+
+
+INSERT INTO hn_reference_code (reference, label) SELECT ref_id, 'FONCTION_SI' FROM hn_reference_has_parent WHERE ref_parent_id = 2100;
