0bdad371a879b576ae241a1005190128d880dbdb
Guillaume <gv.vermeersch@gmail.com>
2017-09-21 12:17:38 +0200

    Base link to span

--- script-sql/bc06-front-news.sql
+++ script-sql/bc06-front-news.sql
@@ -1,1 +1,2 @@
+UPDATE core_menu_item SET itm_route = NULL, itm_route_parameters = NULL WHERE itm_route = 'hopitalnumerique_communautepratique_accueil_index' AND itm_parent IS NULL;
 INSERT INTO core_menu_item (itm_parent, mnu_menu, itm_name, itm_route, itm_display_children, itm_role, itm_order, itm_display) SELECT itm_id, 3, 'Actualité', 'hopitalnumerique_communautepratique_news_index', 0, 'IS_AUTHENTICATED_ANONYMOUSLY', 1, 1 FROM core_menu_item WHERE itm_route = 'hopitalnumerique_communautepratique_accueil_index' AND itm_parent IS NULL
