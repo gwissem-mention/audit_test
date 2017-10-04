10d4a747ae0bf2e4b80668d8c526f4c3d222ac79
Guillaume <gv.vermeersch@gmail.com>
2017-10-04 16:11:41 +0200

    CDP Discussion cart item

---
+++ src/HopitalNumerique/CartBundle/Model/Item/CDPDiscussion.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace HopitalNumerique\CartBundle\Model\Item;
+
+
+use HopitalNumerique\CommunautePratiqueBundle\Entity\Discussion\Discussion;
+
+class CDPDiscussion extends Item
+{
+    /**
+     * @var Discussion $discussion
+     */
+    protected $discussion;
+
+    /**
+     * Publication constructor.
+     *
+     * @param Discussion $discussion
+     */
+    public function __construct(Discussion $discussion)
+    {
+        $this->discussion = $discussion;
+    }
+
+    /**
+     * @return Discussion
+     */
+    public function getObject()
+    {
+        return $this->discussion;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->discussion->getTitle();
+    }
+
+    /**
+     * @return string
+     */
+    public function getObjectType()
+    {
+        return \HopitalNumerique\CartBundle\Entity\Item::CDP_DISCUSSION_TYPE;
+    }
+
+    /**
+     * @return int
+     */
+    public function getObjectId()
+    {
+        return $this->discussion->getId();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRoute()
+    {
+        return 'hopitalnumerique_communautepratique_discussions_public_desfult_discussion';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRouteParameters()
+    {
+        return [
+            'discussion' => $this->discussion->getId(),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getDomains()
+    {
+        return $this->discussion->getDomains();
+    }
+}
