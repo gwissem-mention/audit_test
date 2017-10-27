32a0642383f6bd44818553794a52d89040c9e376
Guillaume <gv.vermeersch@gmail.com>
2017-10-27 09:54:19 +0200

    Combine sql scripts

--- script-sql/bc06-subscription-mail.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO hn_develop.core_mail (mail_id, mail_objet, mail_description, mail_expediteur_mail, mail_expediteur_name, mail_body, mail_params, mail_notification_region_referent) VALUES (81, '[%subjectDomaine] - Nouvelle réponse dans la communauté de pratique', 'Réponse dans une discussion de la communauté de pratique', '%mailContactDomaineCurrent', 'ANAP - %nomContactDomaineCurrent', 'Bonjour,
-
-%nomUtilisateur %prenomUtilisateur vient de poster une réponse dans la discussion "%discussionName" :
-%urlDiscussion
-
-Cordialement,', '{"%nomUtilisateur":"Nom de l''utilisateur", "%prenomUtilisateur":"Prénom de l''utilisateur", "%discussionName":"Nom de la discussion", "%urlDiscussion":"URL de la discussion"}', 0);
