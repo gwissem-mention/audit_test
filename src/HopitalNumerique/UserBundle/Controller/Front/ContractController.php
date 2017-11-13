0e24c623ce13497714a350b246a039f73fdb0ad5
Guillaume <gv.vermeersch@gmail.com>
2017-11-13 08:58:54 +0100

    File exists error

--- src/HopitalNumerique/UserBundle/Controller/Front/ContractController.php
+++ src/HopitalNumerique/UserBundle/Controller/Front/ContractController.php
@@ -5,7 +5,9 @@ namespace HopitalNumerique\UserBundle\Controller\Front;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use HopitalNumerique\UserBundle\Entity\Contractualisation;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Contract controller.
@@ -18,10 +20,18 @@ class ContractController extends Controller
      *
      * @Security("is_granted('download', contract)")
      *
-     * @return BinaryFileResponse
+     * @return BinaryFileResponse|RedirectResponse
      */
     public function downloadAction(Contractualisation $contract)
     {
+        $filesystem = new Filesystem();
+
+        if (!$filesystem->exists($contract->getAbsolutePath())) {
+            $this->addFlash('danger', $this->get('translator')->trans('download.error.file_not_found', [], 'contract'));
+
+            return new RedirectResponse($this->generateUrl('account_profile').'#?contract');
+        }
+
         return new BinaryFileResponse($contract->getAbsolutePath());
     }
 }
