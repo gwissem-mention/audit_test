660ee3cd87fd4788625118dbdbc7dccb71552c2e
Da Fonseca Emmanuel <edafonseca@nodevo.com>
2017-11-29 11:25:13 +0100

    Migrate documents on group comments

--- src/HopitalNumerique/CommunautePratiqueBundle/Command/MigrateCDPCommentCommand.php
+++ src/HopitalNumerique/CommunautePratiqueBundle/Command/MigrateCDPCommentCommand.php
@@ -5,11 +5,14 @@ namespace HopitalNumerique\CommunautePratiqueBundle\Command;
 use HopitalNumerique\CommunautePratiqueBundle\Entity\Commentaire;
 use HopitalNumerique\CommunautePratiqueBundle\Entity\Discussion\Discussion;
 use HopitalNumerique\CommunautePratiqueBundle\Entity\Discussion\Message;
-use HopitalNumerique\CommunautePratiqueBundle\Entity\Fiche;
+use HopitalNumerique\CommunautePratiqueBundle\Entity\Document;
 use HopitalNumerique\CommunautePratiqueBundle\Entity\Groupe;
+use HopitalNumerique\FichierBundle\Entity\File;
+use HopitalNumerique\FichierBundle\Service\FilePathFinder;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Filesystem\Filesystem;
 
 class MigrateCDPCommentCommand extends ContainerAwareCommand
 {
@@ -28,7 +31,6 @@ class MigrateCDPCommentCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $entityManager = $this->getContainer()->get('doctrine.orm.default_entity_manager');
-        $commentRepository = $this->getContainer()->get('hopitalnumerique_communautepratique.repository.commentaire');
         $groupRepository = $this->getContainer()->get('hopitalnumerique_communautepratique.repository.group');
 
         $output->writeln('***************************');
@@ -61,6 +63,8 @@ class MigrateCDPCommentCommand extends ContainerAwareCommand
                 $message->setCreatedAt($comment->getDateCreation());
 
                 $entityManager->persist($message);
+
+                $this->addDocuments($comment, $message);
             }
 
             $output->write(' => ');
@@ -68,6 +72,66 @@ class MigrateCDPCommentCommand extends ContainerAwareCommand
             $entityManager->flush();
 
             $output->writeln('[SAVED]');
+        }
+    }
+
+    /**
+     * @param Commentaire $comment
+     * @param Message $message
+     */
+    private function addDocuments(Commentaire $comment, Message $message)
+    {
+        $entityManager = $this->getContainer()->get('doctrine.orm.default_entity_manager');
+        $filePathFinder = $this->getContainer()->get(FilePathFinder::class);
+        $documentRepository = $entityManager->getRepository(Document::class);
+        $router = $this->getContainer()->get('router');
+        $filesystem = new Filesystem();
+        $baseDir = $this->getContainer()->getParameter('kernel.root_dir');
+
+        $documentLink = '/\/communaute-de-pratiques\/document\/document\/([\d]+)\/download/';
+
+        if (0 === preg_match_all($documentLink, $message->getContent(), $documents)) {
+            return;
+        }
+
+        if (count($documents[0])) {
+            $entityManager->flush();
+
+            foreach ($documents[0] as $k => $v) {
+                /** @var Document $document */
+                $document = $documentRepository->find($documents[1][$k]);
+
+                if (!$document) {
+                    continue;
+                }
+
+                $path = $baseDir.'/../'.$document->getPathname();
+
+                if (!$filesystem->exists($path)) {
+                    continue;
+                }
+
+                $name = uniqid().'.'.$document->getExtension();
+                $file = new File($document->getLibelle(), $name, $comment->getUser());
+                $file->setActive(true);
+
+                $filesystem->copy($path, $filePathFinder->getUserFolderPath($comment->getUser()).'/'.$name);
+
+                $entityManager->persist($file);
+                $message->addFile($file);
+
+                $entityManager->flush();
+
+                $uri = $router->generate(
+                    'hopitalnumerique_communautepratique_discussions_discussion_message_file',
+                    [
+                        'message' => $message->getId(),
+                        'file' => $file->getId(),
+                    ]
+                );
+
+                $message->setContent(str_replace($v, $uri, $message->getContent()));
+            }
         }
     }
 }
